555593bbbf5d43ca18bcb7c65fe5e73ce6b4cf8c
Anirban Roy Das <anirban.nick@gmail.com>
2017-03-28 20:17:40 +0530

    Add docker-entrypoint script to run/test app on docker image run

---
+++ docker-entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -e
+
+if [ "$ENV" = "DEV" ]; then
+	echo "Running Development Application"
+	pip install --no-deps -e .
+	exec python ci_testing_python/app/identidock.py
+
+elif [ "$ENV" = "UNIT_TEST" ]; then
+	echo "Running Unit Tests"
+	pip install --no-deps .
+	exec pytest -v tests/unit 
+
+elif [ "$ENV" = "CONTRACT_TEST" ]; then
+	echo "Running Contract Tests"
+	pip install --no-deps .
+	exec pytest -v tests/contract
+
+elif [ "$ENV" = "INTEGRATION_TEST" ]; then
+	echo "Running Integration Tests"
+	pip install --no-deps .
+	exec pytest -v tests/integration
+
+elif [ "$ENV" = "COMPONENT_TEST" ]; then
+	echo "Running Component Tests"
+	pip install --no-deps .
+	exec uwsgi --http 0.0.0.0:9090 --wsgi-file ci_testing_python/app/identidock.py --callable app --stats 0.0.0.0:9191  --py-autoreload 1
+
+elif [ "$ENV" = "END_TO_END_TESTING" ]; then
+	echo "Running End_To_End Tests"
+	pip install --no-deps .
+	exec uwsgi --http 0.0.0.0:9090 --wsgi-file ci_testing_python/app/identidock.py --callable app --stats 0.0.0.0:9191  --py-autoreload 1
+
+elif [ "$ENV" = "PROD" ]; then 
+	echo "Running Production Application"
+	pip install --no-deps .
+	exec uwsgi --http 0.0.0.0:9090 --wsgi-file ci_testing_python/app/identidock.py --callable app --stats 0.0.0.0:9191  --py-autoreload 1
+
+else
+	echo "Please provide an environment"
+	echo "Stopping"
+fi
