b0d69849e474f54a654ab2a8ada8cf8d447a02ab
Anirban Roy Das <anirban.nick@gmail.com>
2017-03-30 03:35:10 +0530

    Add -v -s switches to pytest commands

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,24 +9,24 @@ if [ "$ENV" = "DEV" ]; then
 elif [ "$ENV" = "UNIT_TEST" ]; then
 	echo "Running Unit Tests"
 	pip install --no-deps .
-	exec pytest -v tests/unit 
+	exec pytest -v -s tests/unit 
 
 elif [ "$ENV" = "CONTRACT_TEST" ]; then
 	echo "Running Contract Tests"
 	pip install --no-deps .
-	exec pytest -v tests/contract
+	exec pytest -v -s tests/contract
 
 elif [ "$ENV" = "INTEGRATION_TEST" ]; then
 	echo "Running Integration Tests"
 	pip install --no-deps .
-	exec pytest -v tests/integration
+	exec pytest -v -s tests/integration
 
 elif [ "$ENV" = "COMPONENT_TEST" ]; then
 	echo "Running Component Tests"
 	pip install --no-deps .
-	exec uwsgi --http 0.0.0.0:9090 --wsgi-file ci_testing_python/app/identidock.py --callable app --stats 0.0.0.0:9191  --py-autoreload 1
+	exec python ci_testing_python/app/identidock.py
 
-elif [ "$ENV" = "END_TO_END_TESTING" ]; then
+elif [ "$ENV" = "END_TO_END_TEST" ]; then
 	echo "Running End_To_End Tests"
 	pip install --no-deps .
 	exec uwsgi --http 0.0.0.0:9090 --wsgi-file ci_testing_python/app/identidock.py --callable app --stats 0.0.0.0:9191  --py-autoreload 1
