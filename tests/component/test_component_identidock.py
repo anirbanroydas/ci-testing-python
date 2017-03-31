209d3e1ff6f24fef957c72bbada7161388c946a3
Anirban Roy Das <anirban.nick@gmail.com>
2017-03-31 12:49:39 +0530

    Comment out time.sleep command and its import statement

--- tests/component/test_component_identidock.py
+++ tests/component/test_component_identidock.py
@@ -1,6 +1,6 @@
 import pytest
 import requests
-from time import sleep
+# from time import sleep
 
 COMPONENT_INDEX_URL = "http://identidock:5000"
 
@@ -10,8 +10,8 @@ COMPONENT_MONSTER_BASE_URL = COMPONENT_INDEX_URL + '/monster'
 
 
 def test_get_mainpage():
-	print('component tester sleeping for 1 sec to let the identidock app to be ready adn also start its server')
-	sleep(1)
+	# print('component tester sleeping for 1 sec to let the identidock app to be ready adn also start its server')
+	# sleep(1)
 	page = requests.get(COMPONENT_INDEX_URL)
 	assert page.status_code == 200
 	assert 'Joe Bloggs' in str(page.text)
