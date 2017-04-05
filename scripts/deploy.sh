be3d8ce6f91e97f8e011cc99cc79590e2b427235
Anirban Roy Das <anirban.nick@gmail.com>
2017-04-05 11:25:37 +0530

    Add condition on whether to deploy or not depending on one env variable

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,9 @@ DOCKER_COMPOSE="docker-compose"
 
 echo "CI SERVER : $1"
 echo "Deploy Environment: $2"
+echo "DEPLOY_PRODUCTION : $DEPLOY_PRODUCTION"
+echo "env variables :"
+printenv
 
 if [ "$1" = "jenkins" ]; then
     DOCKER="sudo docker"
@@ -19,18 +22,23 @@ SERVER_COUNT=0
 DEPLOY_SUCCESS=1
 SERVICE_NAME=identidock
 
-while [ "$DEPLOY_SUCCESS" -gt 0 ];
-do
-	SERVER_COUNT = SERVER_COUNT + 1
+if [ "$DEPLOY_PRODUCTION" = "yes" ]; then
 
-	if [ $SERVER_COUNT -eq $((MANAGER_COUNT+1)) ]; then
-		echo "Unable to update service in any of the managers"
-		break
-	fi
+	while [ "$DEPLOY_SUCCESS" -gt 0 ];
+	do
+		SERVER_COUNT = SERVER_COUNT + 1
 
-	SERVER="MANAGER_$SERVER_COUNT"
-	ssh $SERVER_USER@${!SERVER} $DOCKER service update --image $DOCKER_REPO:$DEPLOY_IDENTIDOCK_IMAGE_TAG $SERVICE_NAME
+		if [ $SERVER_COUNT -eq $((MANAGER_COUNT+1)) ]; then
+			echo "Unable to update service in any of the managers"
+			break
+		fi
 
-	DEPLOY_SUCCESS=$(echo $?);	
-	echo "DEPLOY SUCCESS : $DEPLOY_SUCCESS"
-done
+		SERVER="MANAGER_$SERVER_COUNT"
+		ssh $SERVER_USER@${!SERVER} $DOCKER service update --image $DOCKER_REPO:$DEPLOY_IDENTIDOCK_IMAGE_TAG $SERVICE_NAME
+
+		DEPLOY_SUCCESS=$(echo $?);	
+		echo "DEPLOY SUCCESS : $DEPLOY_SUCCESS"
+	done
+else
+	echo "Deploy cannot happen due to non matching condition of DEPLOY_PRODUCTION environment variable"
+fi
